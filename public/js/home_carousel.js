b67b6f722510405a82b48e14bfbcd9a8c816696b
AndreiChornii <andrei.chornii@gmail.com>
2020-07-19 10:07:53 +0300

    carousel_with_buttons__with_names_ok

--- public/js/home_carousel.js
+++ public/js/home_carousel.js
@@ -2,6 +2,7 @@
 
 var video = {};
 video.vid = document.getElementById("myVideo");
+video.names = document.getElementById("video_name");
 video.checkBrowser = function (src) {
     let isSupp = this.vid.canPlayType("video/mp4");
     if (isSupp === "") {
@@ -12,16 +13,27 @@ video.checkBrowser = function (src) {
     console.log(isSupp);
     this.vid.load();
 };
+video.setName = function(name) {
+    console.log(this.names); 
+    console.log(name);
+    this.names.innerText = name;
+};
 video.prev_btn = document.querySelector('#prev');
 video.next_btn = document.querySelector('#next');
 
 let video_addr_name = [];
 let arrVideos = ['http://sobor/video/ar_1.mp4', 'http://sobor/video/ar_2.mp4', 'http://sobor/video/ar_3.mp4'];
+let arrNames = ['video_1', 'video_2', 'video_3'];
 video_addr_name['movies'] = arrVideos;
+video_addr_name['names'] = arrNames;
 console.dir(video_addr_name);
 video.arrVideos = video_addr_name['movies'];
+video.arrNames = video_addr_name['names'];
 
-video.checkBrowser("http://sobor/video/ar_1.mp4");
+let ini = function(){
+    video.checkBrowser("http://sobor/video/ar_1.mp4");
+    video.setName("video_1");
+};
 
 video.prev_btn.onclick = function(){
     console.dir(video);
@@ -40,6 +52,7 @@ video.prev_btn.onclick = function(){
     }
     
     video.checkBrowser(video.arrVideos[idx]);
+    video.setName(video.arrNames[idx]);
 };
 
 video.next_btn.onclick = function(){
@@ -60,58 +73,7 @@ video.next_btn.onclick = function(){
     }
     
     video.checkBrowser(video.arrVideos[idx]);
+    video.setName(video.arrNames[idx]);
 };
 
-
-
-//function prev(){
-//    
-//    var cr_video_src = container.children[0].children[0].src;
-//    console.log(cr_video_src);
-//    var idx = videos.indexOf(cr_video_src);
-//    console.log(idx);
-//    console.log(videos.length);
-//    if (idx ===  videos.length - 1 ) {
-//        idx = 0;
-//        console.log("ravno");
-//        console.log(videos[idx]);
-//    }else{
-//        idx++;
-//        console.log("ne_ravno");
-//        console.log(videos[idx]);
-//    }
-//    
-//    container.children[0].children[0].src = videos[idx];
-//    container.load();
-//}
-//
-//function next(){
-//    var cr_video_src = container.children[0].children[0].src;
-//    console.log(cr_video_src);
-//    var idx = videos.indexOf(cr_video_src);
-//    console.log(idx);
-//    console.log(videos.length);
-//    if (idx ===  0 ) {
-//        idx = videos.length - 1;
-//        console.log("ravno");
-//        console.log(videos[idx]);
-//    }else{
-//        idx--;
-//        console.log("ne_ravno");
-//        console.log(videos[idx]);
-//    }
-//    
-//    container.children[0].children[0].src = videos[idx];
-//    container.load();
-//}
-//
-//var videos = ['http://sobor/video/ar_1.mp4', 'http://sobor/video/ar_2.mp4', 'http://sobor/video/ar_3.mp4'];
-//
-//console.log(videos.length);
-//
-//var vid = document.getElementById("myVideo");
-//
-//var prev_btn = document.querySelector('#prev');
-//prev_btn.onclick = prev;
-//var next_btn = document.querySelector('#next');
-//next_btn.onclick = next;
+ini();
