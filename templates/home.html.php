d73070bbff544c1d28a1338137254f3f86ad243e
AndreiChornii <andrei.chornii@gmail.com>
2020-07-19 17:57:56 +0300

    home_ok

--- templates/home.html.php
+++ templates/home.html.php
@@ -1,16 +1,25 @@
 <div id="cont">
     <button id="prev" type="button">&#10094;</button>
     <div id="output_div">
-        
+
         <video id="myVideo" width="640" height="360" controls="controls" >
             Your browser does not support HTML5 video.
         </video>
-        
+
         <!-- Next and previous buttons -->
-<!--        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>-->
+        <!--        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>-->
     </div>
     <button id="next" type="button">&#10095;</button>
 </div>
 <div id="video_name">video_name</div>
-<div id="description">description</div>
+<div id="description">
+    <h2>Патріотичне виховання молоді</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin egestas, justo in posuere blandit, neque nulla euismod eros, ac pulvinar libero arcu a orci. Ut ullamcorper ut metus ac vulputate. Sed turpis augue, gravida quis diam tempus, maximus tempor nibh. Fusce tristique ullamcorper ipsum, at pretium erat ultrices a. Proin ut nibh metus. Sed non urna velit. In hac habitasse platea dictumst. Nullam cursus mi nibh, id luctus tellus bibendum nec. Sed sollicitudin neque mi, accumsan dapibus purus luctus sit amet. Nullam quis facilisis ligula, a euismod leo. Donec mauris lorem, ultricies vitae faucibus nec, sollicitudin et ante. Curabitur vitae eleifend ipsum. Ut eget sodales tellus. Praesent et molestie leo. Praesent eget turpis arcu. Proin vitae tincidunt lorem.
+
+        Phasellus faucibus id magna id dignissim. Nulla arcu ex, pulvinar in rhoncus a, blandit sed nibh. Vivamus tortor quam, cursus quis turpis et, ornare consequat felis. Praesent malesuada vehicula odio, id lobortis nibh sagittis et. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur dignissim arcu quam, id vestibulum magna viverra vel. In bibendum, leo sit amet sodales condimentum, diam massa molestie sem, ac mollis ipsum nisl ut tortor. Cras efficitur ex ut est mollis ultricies. Mauris sed aliquet mi. Nam nec enim nec odio lobortis fermentum. Proin auctor neque id dapibus vulputate. Nunc vel commodo quam. Nullam volutpat, mauris nec blandit rutrum, est velit dapibus nibh, non vestibulum lacus metus nec massa. Mauris et vehicula erat, in iaculis justo.
+
+        Proin rhoncus ligula odio, at bibendum arcu dapibus et. Integer feugiat sed eros quis mattis. Mauris in sapien et nisl fringilla tempor sit amet id urna. Suspendisse potenti. Suspendisse potenti. Cras commodo leo eget orci commodo varius. Etiam consectetur pharetra elit ac tempor. Cras fringilla erat ac odio congue egestas. Suspendisse sit amet lorem at lorem cursus pulvinar laoreet sit amet mauris. Pellentesque blandit tortor quis dictum fringilla. Phasellus nec laoreet eros. Nam nunc ex, iaculis vel pellentesque lacinia, rutrum ac eros. Donec quis vehicula neque, et accumsan massa. 
+    </p>
+</div>
