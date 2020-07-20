b266cda85ef24d37ebbee4198c5c5358e104f953
AndreiChornii <andrei.chornii@gmail.com>
2020-07-20 15:12:38 +0300

    try_6

--- public/js/home_carousel.js
+++ public/js/home_carousel.js
@@ -5,18 +5,19 @@ video.vid = document.getElementById("myVideo");
 video.names = document.getElementById("video_name");
 video.checkBrowser = function (src) {
     let isSupp = this.vid.canPlayType("video/mp4");
+    let src_for_video_obj = '../video/' + src.substring(src.length - 8, src.length);
     if (isSupp === "") {
-        this.vid.src = "http://sobor/video/ar_1.webm";
+        this.vid.src = "../video/ar_1.webm";
     } else {
-        this.vid.src = src;
+        this.vid.src = src_for_video_obj;
 //          let source = document.createElement('source');
-//          console.log(source);
+        console.log(src_for_video_obj);
 //        this.vid.;
     }
     console.dir(this.vid);
     console.log(isSupp);
     this.vid.load();
-    this.vid.play();
+//    this.vid.play();
 };
 video.setName = function(name) {
     console.log(this.names); 
@@ -27,8 +28,8 @@ video.prev_btn = document.querySelector('#prev');
 video.next_btn = document.querySelector('#next');
 
 let video_addr_name = [];
-//let arrVideos = ['http://sobor/video/ar_1.mp4', 'http://sobor/video/ar_2.mp4', 'http://sobor/video/ar_3.mp4'];
-let arrVideos = ['../video/ar_1.mp4', '../video/ar_2.mp4', '../video/ar_3.mp4'];
+let arrVideos = ['http://sobor/video/ar_1.mp4', 'http://sobor/video/ar_2.mp4', 'http://sobor/video/ar_3.mp4'];
+//let arrVideos = ['../video/ar_1.mp4', '../video/ar_2.mp4', '../video/ar_3.mp4'];
 let arrNames = ['video_1', 'video_2', 'video_3'];
 video_addr_name['movies'] = arrVideos;
 video_addr_name['names'] = arrNames;
