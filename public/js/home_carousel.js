f0f65ea867af3eb4aaf2ecbcd44c3f16be3d7965
AndreiChornii <andrei.chornii@gmail.com>
2020-07-20 10:24:08 +0300

    autoplay_3

--- public/js/home_carousel.js
+++ public/js/home_carousel.js
@@ -27,7 +27,7 @@ video.prev_btn = document.querySelector('#prev');
 video.next_btn = document.querySelector('#next');
 
 let video_addr_name = [];
-let arrVideos = ['http://sobor/video/ar_1.webm', 'http://sobor/video/ar_2.mp4', 'http://sobor/video/ar_3.mp4'];
+let arrVideos = ['http://sobor/video/ar_1.mp4', 'http://sobor/video/ar_2.mp4', 'http://sobor/video/ar_3.mp4'];
 let arrNames = ['video_1', 'video_2', 'video_3'];
 video_addr_name['movies'] = arrVideos;
 video_addr_name['names'] = arrNames;
@@ -37,7 +37,7 @@ video.arrNames = video_addr_name['names'];
 
 let init = function(){
 //    video.checkBrowser("http://sobor/video/rabbit320.mp4");
-    video.checkBrowser("http://sobor/video/ar_1.webm");
+    video.checkBrowser("http://sobor/video/ar_1.mp4");
     video.setName("video_1");
 };
 
