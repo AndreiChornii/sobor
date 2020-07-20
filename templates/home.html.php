1a07bb0e991544cd069cd6cb99d56bba84fa1ee0
AndreiChornii <andrei.chornii@gmail.com>
2020-07-20 10:54:13 +0300

    autoplay_5

--- templates/home.html.php
+++ templates/home.html.php
@@ -6,9 +6,12 @@
             Your browser does not support HTML5 video.
         </video>
 
-        <!-- Next and previous buttons -->
-        <!--        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                <a class="next" onclick="plusSlides(1)">&#10095;</a>-->
+<!--        <video id="myVideo" width="640" height="360" controls>
+            <source src="rabbit320.mp4" type="video/mp4">
+            <source src="rabbit320.webm" type="video/webm">
+            <p>Your browser doesn't support HTML5 video. Here is a <a href="rabbit320.mp4">link to the video</a> instead.</p>
+        </video>-->
+
     </div>
     <button id="next" type="button">&#10095;</button>
 </div>
