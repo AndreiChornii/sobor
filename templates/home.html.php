41caa86cc0cde5c162dba4e0b2b0310f8affd254
AndreiChornii <andrei.chornii@gmail.com>
2020-07-20 11:08:04 +0300

    autoplay_7

--- templates/home.html.php
+++ templates/home.html.php
@@ -8,6 +8,7 @@
 
         <video id="myVideo" width="640" height="360" controls>
             <source src="http://sobor/video/rabbit.mp4" type="video/mp4">
+            <source src="http://sobor/video/rabbit.webm" type="video/webm">
             <p>Your browser doesn't support HTML5 video. Here is a <a href="rabbit320.mp4">link to the video</a> instead.</p>
         </video>
 
